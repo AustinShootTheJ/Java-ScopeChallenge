42e04b320ae64a8979c4b9f4cf4ee2d41a53a59e
AustinShootTheJ <austinjmccune@gmail.com>
2018-06-20 14:09:57 -0400

    Initial commit

---
+++ src/com/example/scopechallenge/X.java
@@ -0,0 +1,24 @@
+package com.example.scopechallenge;
+
+import java.util.Scanner;
+
+public class X {
+private int x = 0;
+
+
+
+
+    public void x(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number to receive times table.");
+        int x = scanner.nextInt();
+
+        for(this.x = 0; this.x<=12; this.x++){
+            System.out.println( x + " times " + this.x + " is " + x*this.x);
+        }
+
+    }
+
+
+}
